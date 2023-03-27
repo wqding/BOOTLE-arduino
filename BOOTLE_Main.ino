d718ad8f7b42f303fcd06d5dd94c0ea3334893a5
Andy Ren <andy.ren@uwaterloo.ca>
2023-03-27 10:19:15 -0400

    BOOTLE_Main: Demo-ready code
    Fully functional code used for Symposium (March 15th, 2023)
    
    Level sensor needs recalibration with water level upon each use
    (Adjust resistance reading from analog pin).

--- BOOTLE_Main.ino
+++ BOOTLE_Main.ino
@@ -49,6 +49,9 @@ bool bat_charge;
 // STORES CURRENT WEATHER TEMPERATURE
 float weather;
 
+// STORES whether connected to a phone
+bool is_connected;
+
 // the value of the 'other' resistor
 #define SERIESRESISTOR 560    
  
@@ -57,17 +60,25 @@ float weather;
 
 // BLUETOOTH STUFF
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define ANALOG_READ_UUID "6d68efe5-04b6-4a85-abc4-c2670b7bf7fd"
+#define BATTERY_READ_UUID "6d68efe5-04b6-4a85-abc4-c2670b7bf7fd"
+#define TEMPERATURE_READ_UUID "6ade8e90-47fd-41ce-b0fc-8069d8b36656"
+#define HEIGHT_READ_UUID "3b53d5f2-c5f7-4652-aadc-220bcbe2a3a1"
+#define WEATHER_WRITE_UUID "c64387ae-75f1-4194-b83d-bbb261c81f39"
 #define DIGITAL_WRITE_UUID "f27b53ad-c63d-49a0-8c0f-9f297e6cc520"
 
 BLEService newService(SERVICE_UUID); // creating the service
 
-BLEByteCharacteristic heightSend(ANALOG_READ_UUID, BLERead | BLENotify | BLEWrite); // creating the Analog Value characteristic
-BLEByteCharacteristic tempSend(ANALOG_READ_UUID, BLERead | BLENotify | BLEWrite); // creating the Analog Value characteristic
-BLEByteCharacteristic batterySend(ANALOG_READ_UUID, BLERead | BLENotify | BLEWrite); // creating the Analog Value characteristic
+// Characteristics for sending data back to the app
+BLEByteCharacteristic heightSend(HEIGHT_READ_UUID, BLERead | BLENotify | BLEWrite); // creating the Analog Value characteristic
+BLEByteCharacteristic tempSend(TEMPERATURE_READ_UUID, BLERead | BLENotify | BLEWrite); // creating the Analog Value characteristic
+BLEByteCharacteristic batterySend(BATTERY_READ_UUID, BLERead | BLENotify | BLEWrite); // creating the Analog Value characteristic
+
+// Characteristics for receiving data on the device
+BLEByteCharacteristic weatherDisplay(WEATHER_WRITE_UUID, BLERead | BLENotify | BLEWrite); // creating the LED characteristic
 BLEByteCharacteristic displayMode(DIGITAL_WRITE_UUID, BLERead | BLENotify | BLEWrite); // creating the LED characteristic
 
 long previousMillis = 0;
+long previousDisplayMillis = 0;
 
 // Define the battery voltage object
 Battery battery = Battery(3000, 4200, ADC_BATTERY);
@@ -79,7 +90,7 @@ void setup(void)
 {
   // Start serial port to baud rate of 9600
   Serial.begin(9600);
-  while (!Serial) { delay(10); }
+  //while (!Serial) { delay(10); }
 
   // Initialize PMIC I2C API
   if (!PMIC.begin()) {
@@ -114,6 +125,7 @@ void setup(void)
   newService.addCharacteristic(heightSend);
   newService.addCharacteristic(tempSend);
   newService.addCharacteristic(batterySend);
+  newService.addCharacteristic(weatherDisplay);
 
   // Adding the service
   BLE.addService(newService);
@@ -123,6 +135,7 @@ void setup(void)
   heightSend.writeValue(1);
   tempSend.writeValue(1);
   batterySend.writeValue(1);
+  weatherDisplay.writeValue(1);
 
   BLE.advertise(); // Start advertising the service
   Serial.println("Bluetooth device active, waiting for connections...");
@@ -148,6 +161,9 @@ void setup(void)
 
   // set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
   sensors.setResolution(insideThermometer, 9);
+
+  get_data();
+  update_display(0);
 }
 
 // Displays a simple booting message when the bottle powers-up
@@ -207,6 +223,8 @@ void loop(void)
     Serial.println(central.address()); // print the central's BT address
     
     digitalWrite(LED_BUILTIN, HIGH); // turn on the LED to indicate the connection
+    is_connected = true; 
+    update_display(0);
 
     // Main routine while connected to the app
     // App sends data values via button click
@@ -217,35 +235,50 @@ void loop(void)
       long currentMillis = millis();
       
       if (currentMillis - previousMillis >= 1000) { // if 5000ms have passed, we check the A1 pin
-          // It responds almost immediately. Let's print out the data
+        // It responds almost immediately. Let's print out the data
+
+        get_data();
 
         // Get temperature value from sensor
-        sensors.requestTemperatures(); // Send the command to get temperatures
-        printTemperature(insideThermometer); // Use a simple function to print out the data
+        // sensors.requestTemperatures(); // Send the command to get temperatures
+        // printTemperature(insideThermometer); // Use a simple function to print out the data
 
-        // Get water level from the sensor
-        loop_level();
+        // // Get water level from the sensor
+        // //loop_level();
+        // loop_fake();
 
-        // Get battery percentage and charging state
-        getBatteryState();
+        // // Get battery percentage and charging state
+        // getBatteryState();
 
         previousMillis = currentMillis;
 
         // Send data for battery, temperature, and height back to the app
         batterySend.writeValue(bat_percent);
         tempSend.writeValue(tempC);
-        heightSend.writeValue(waterheight);
+        heightSend.writeValue(percentfull);
 
         // weather information API here: 
         // HERE: some call to get it 
 
         // App doesn't have external temperature data, just hardcode -5 C or something
-        weather = -5;
+        //weather = -5;
 
         // Based on button press on app, change the information on the display
         if (displayMode.written()) {
-          Serial.println(displayMode.value());
-          update_display(displayMode.value());
+          int newMode = displayMode.value();
+          //displayMode.readValue(data_read, sizeof(data_read));
+
+          if(newMode == 4) {
+            Serial.println(weatherDisplay.value());
+            int temp_weather = weatherDisplay.value() - 100;
+            weather = 0;
+            weather += (float)temp_weather + (float)(random(-30, 30))/100.00;
+          }
+          //Serial.println(displayMode.value());
+          Serial.println(newMode);
+          Serial.println(weather);
+          // update_display(displayMode.value());
+          update_display(newMode);
         }
 
       }
@@ -256,30 +289,132 @@ void loop(void)
     Serial.println(central.address());
   } else { // if we don't have bluetooth connection, default display to show some information
     long currentMillis = millis();
-    if(currentMillis - previousMillis >= 10000) {
+    if(currentMillis - previousMillis >= 1000) {
 
-      // Get the thermometer temperature
-      sensors.requestTemperatures();
-      printTemperature(insideThermometer);
+      is_connected = false; 
+      get_data();
+      // // Get the thermometer temperature
+      // sensors.requestTemperatures();
+      // printTemperature(insideThermometer);
 
-      // Get the water level
-      loop_level();
+      // // Get the water level
+      // //loop_level();
+      // loop_fake();
 
-      // Get battery state and charging state
-      getBatteryState();
+      // // Get battery state and charging state
+      // getBatteryState();
 
-      // Actual outside weather
-      weather = -5;
-      Serial.print("Weather: ");
-      Serial.print(weather);
-      Serial.print((char)248);
-      Serial.println("C");
+      // // Actual outside weather
+      // Serial.print("Weather: ");
+      // Serial.print(weather);
+      // Serial.print((char)248);
+      // Serial.println("C");
       
       previousMillis = currentMillis;
-      update_display(4);
+    }
+
+    if(currentMillis - previousDisplayMillis >= 10000) {
+        previousDisplayMillis = currentMillis;
+        update_display(0);
     }
   }
 
+}
+
+void get_data() {
+  // Get the thermometer temperature
+  sensors.requestTemperatures();
+  printTemperature(insideThermometer);
+
+  // Get the water level
+  //loop_level();
+  //loop_fake();
+  water_level();
+
+  // Get battery state and charging state
+  getBatteryState();
+
+  // Actual outside weather
+  Serial.print("Weather: ");
+  Serial.print(weather);
+  Serial.print((char)248);
+  Serial.println("C");
+}
+
+void loop_fake() {
+  waterheight = 2.00; 
+
+  waterheight += (float)(random(-40, 40) / 100.00);
+
+  percentfull = (waterheight/5.0) * 100;
+}
+
+// float resistances[] = {610.00, 570.00, 525.00, 490.00, 470.00, 455.00}; 
+// float heights[] = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0};
+
+float resistances[] = {900.00, 860.00, 820.00, 680.00, 590.00, 550.00}; 
+float heights[] = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0};
+
+float lin_interpolation(float x) {
+      // find the two points that x lies between
+    int i = 0;
+    while (i < 5 && x < resistances[i + 1]) {
+        i++;
+    }
+
+    if (i < 5 && abs(x - resistances[i + 1]) <= 40.0) {
+      return heights[i + 1];
+    }
+    
+    // perform linear interpolation using the two points
+    float x0 = resistances[i];
+    float y0 = heights[i];
+
+    float x1 = resistances[i+1];
+    float y1 = heights[i+1];
+
+    float res = y0 + (x - x0) * ((y1 - y0)/(x1 - x0));
+
+    return res;
+}
+
+int lowerLimit = 500;   // Sensor completely in the water
+int upperLimit = 900; // 610;   // Sensor completely dry
+float sensorlength = 12.7;  // in cm (around 5 inches)
+
+float resistance = 0.0;
+
+void water_level() {
+  
+  float inputValue = analogRead(SENSORPIN);
+  resistance = inputValue;
+
+  float result = 0.0;
+  if(inputValue > lowerLimit && inputValue < upperLimit)
+  {
+    // calculate Range
+    // float range = upperLimit - lowerLimit;
+    // // offset input Value
+    // inputValue -= lowerLimit;
+    // // calculate height
+    // result = sensorlength * inputValue/range;
+    // result = sensorlength - result;
+    
+    result = lin_interpolation(inputValue);
+  } 
+  else if(inputValue >= upperLimit)
+  {
+    // set min
+    result = 0.0;   
+  } 
+  else if(inputValue <= lowerLimit)
+  {
+    // set max
+    result = sensorlength / 2.54;
+  }
+  
+  waterheight = result;
+  percentfull = waterheight/5 *100;
 }
 
 void loop_level()
@@ -297,10 +432,15 @@ void loop_level()
   waterheight = SERIESRESISTOR / waterheight;
 //
 //  Serial.print("Sensor resistance "); 
-//  Serial.println(waterheight);
+  Serial.println(waterheight);
   
 //  Serial.println(voltage);
-  waterheight = 5-((waterheight-640)/140);
+  //waterheight = 5-((waterheight-640)/140);
+  //waterheight = 15 + waterheight * (5.5-1)/(1450-2200);
+
+  // Serial.println( (4*(waterheight - 870))/522);
+  Serial.println( (4*(waterheight - 870))/430 + 3);
+  waterheight = 5 - (4*(waterheight - 870))/430 +3;
 
   Serial.print("Water height (inches): "); 
   Serial.println(waterheight);
@@ -322,62 +462,24 @@ void update_display(int mode){
     display.setTextSize(2);
     display.setCursor(0, 0);
     display.setTextColor(EPD_BLACK);
-    display.print("Height: ");
-    display.print(waterheight);
-    display.println("");
-    display.display();
-  } else if(mode == 1){
-    display.clearBuffer();
-    display.setTextSize(3);
-    display.setCursor(0, 0);
-    display.setTextColor(EPD_BLACK);
-    display.print("Full: ");
-    display.print(percentfull);
-    display.println("");
-    display.display();
-  } else if(mode == 2){
-    display.clearBuffer();
-    display.setTextSize(3);
-    display.setCursor(0, 0);
-    display.setTextColor(EPD_BLACK);
-    display.print("Temp: ");
-    display.print(tempC);
-    display.println("");
-    display.display();
-  } else if(mode == 3){
-    display.clearBuffer();
-    display.setTextSize(2);
-    display.setCursor(0, 0);
-    display.setTextColor(EPD_BLACK);
+    if(is_connected) {
+      display.print("Weather ");
+      display.print(weather);
+      display.print((char)248);
+      display.print("C");
+      display.println("");
+    }
     display.print("Water Temp: ");
     display.print(tempC);
-    display.println("");
-    display.print("Percent Full: ");
-    display.print(percentfull);
-    display.println("");
-    display.print("Water Height: ");
-    display.print(waterheight);
-    display.println("");
-    display.display();
-  } else { // Default print everything
-    display.clearBuffer();
-    display.setTextSize(2);
-    display.setCursor(0, 0);
-    display.setTextColor(EPD_BLACK);
-    display.print("Weather ");
-    display.print(weather);
     display.print((char)248);
     display.print("C");
     display.println("");
-    display.print("Water Temp: ");
-    display.print(tempC);
-    display.println("");
     display.print("Percent Full: ");
     display.print(percentfull);
-    display.println("");
+    display.println("%");
     display.print("Water Height: ");
     display.print(waterheight);
-    display.println("");
+    display.println(" in");
     display.print("Battery Capacity: ");
     display.print(bat_percent);
     display.print("%");
@@ -385,6 +487,70 @@ void update_display(int mode){
     display.print("Charging: ");
     display.print(bat_charge ? "true" : "false");
     display.println("");
+    display.print("Connected to app: ");
+    display.print(is_connected ? "Yes" : "No");
+    display.println("");
+    // display.print("Debug level resistance: ");
+    // display.print(resistance);
+    // display.println("");
+    display.display();
+    // display.clearBuffer();
+    // display.setTextSize(2);
+    // display.setCursor(0, 0);
+    // display.setTextColor(EPD_BLACK);
+    // display.print("Height: ");
+    // display.print(waterheight);
+    // display.println("");
+    // display.display();
+  } else if (mode == 1) {
+    display.clearBuffer();
+    display.setTextSize(2);
+    display.setCursor(0, 0);
+    display.setTextColor(EPD_BLACK);
+    display.print("Battery Capacity: ");
+    display.print(bat_percent);
+    display.print("%");
+    display.println("");
+    display.print("Charging: ");
+    display.print(bat_charge ? "true" : "false");
+    display.println("");
+    display.display();
+  } else if(mode == 2) {
+    display.clearBuffer();
+    display.setTextSize(3);
+    display.setCursor(0, 0);
+    display.setTextColor(EPD_BLACK);
+    display.print("Full: ");
+    display.print(percentfull);
+    display.println("%");
+    display.display();
+  } else if(mode == 3) {
+    display.clearBuffer();
+    display.setTextSize(3);
+    display.setCursor(0, 0);
+    display.setTextColor(EPD_BLACK);
+    display.print("Temp: ");
+    display.print(tempC);
+    display.print((char)248);
+    display.print("C");
+    display.println("");
+    display.display();
+  } else if(mode == 4) {
+    display.clearBuffer();
+    display.setTextSize(2);
+    display.setCursor(0, 0);
+    display.setTextColor(EPD_BLACK);
+    display.print("Weather: ");
+    display.print(weather);
+    display.print((char)248);
+    display.print("C");
+    display.println("");
+    // display.print("Percent Full: ");
+    // display.print(percentfull);
+    // display.println("");
+    // display.print("Water Height: ");
+    // display.print(waterheight);
+    // display.println("");
     display.display();
   }
 //  delay(180000);
